9f5936b3b38f17fd8868117b203243e0c95bcee5
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-02-06 15:59:59 +0100

    success to use database test

--- index.php
+++ index.php
@@ -5,4 +5,17 @@
     Version: 1.0
 */
 
+try {
+    $user = 'ICT-151';
+    $password = 'eh bah non';
+    $dbh = new PDO('mysql:host=localhost;dbname=mcu', $user, $password);
+    foreach ($dbh->query('SELECT * from actors') as $row) {
+        print_r($row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
 ?>
