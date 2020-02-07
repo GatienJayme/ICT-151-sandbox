65fdc9864a189411b7c4354a0a64f264896d1c45
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-02-07 09:37:37 +0100

    quelques test unitaire pour tester si j'ai bien compris la synchro avec database

--- index.php
+++ index.php
@@ -5,17 +5,54 @@
     Version: 1.0
 */
 
-try {
-    $user = 'ICT-151';
-    $password = 'eh bah non';
-    $dbh = new PDO('mysql:host=localhost;dbname=mcu', $user, $password);
-    foreach ($dbh->query('SELECT * from actors') as $row) {
-        print_r($row);
+function getAllItems()
+{
+    require '.const.php';
+    try {
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $password);
+        $query = 'SELECT * FROM actors';
+        $statement = $dbh->prepare($query); // prepare query
+        $statement->execute(); // execute query
+        $queryResult = $statement->fetchAll(); // prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        return null;
     }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
 }
 
+// Test unitaire de la fonction getAllItems
+/*$items = getAllItems();
+if(count($items) == 11) {
+    echo"Réussi !!!!";
+}
+else {
+    echo "BUG !!!!!!";
+}*/
+// fonction qui nous redonne un personnage précis en fonction de son nom ou son id
+function getItem($id)
+{
+    require '.const.php';
+    try {
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $password, $id);
+        $query = 'SELECT * FROM actors where id = $id';
+        $statement = $dbh->prepare($query); // prepare query
+        $statement->execute(); // execute query
+        $queryResult = $statement->fetch(); // prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
+// Test unitaire de la fonction geItem
+$id = getItem($id);
+if (getItem($id)[$id] == getItem($id)[$user]) {
+    echo "OK !!!";
+} else {
+    echo "PAS OK !!!";
+}
 ?>
