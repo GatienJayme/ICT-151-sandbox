13d537f2412a04416a6c36da06e7f5714d209b7e
GatienJayme <47849706+GatienJayme@users.noreply.github.com>
2020-02-27 15:59:39 +0100

    delete add and update good but not test

--- crud.php
+++ crud.php
@@ -30,7 +30,21 @@ try {
 // Permet de créer un réalisateur
 function createFilmMaker($filmMaker)
 {
-
+    try {
+        $dbh = getPDO();
+        $query = 'SELECT * FROM filmmakers
+INSERT INTO filmmakers (id, filmmakersnumber, lastname, firstname, birthname, nationality)
+VALUES (4, 424, \'Luc-Franky\', \'Bernard\', \'1976-01-12\', \'Asiatique\')
+SELECT * FROM filmmakers';
+        $statement = $dbh->prepare($query); // prepare query
+        $statement->execute(); // execute query
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC); // prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        return null;
+    }
 }
 
 //
@@ -51,16 +65,44 @@ function getFilmMakerByName($name)
 
 }
 
-// Permet de modifier un film identifier par son nom
+// Permet de modifier un film identifier par filmMaker
 function updateFilmMaker($filmMaker)
 {
-
+    try {
+        $dbh = getPDO();
+        $query = "update filmmakers SET
+    filmmakersnumber =: filmmakersnumber,
+    firstname =: firstname,
+    lastname =: lastname,
+    birthname =: birthname,
+    nationality =: nationality
+    where id =: id";
+        $statement = $dbh->prepare($query);
+        $statement->execute($filmMaker);
+        $dbh = null;
+        return true;
+    } catch (PDOException $e) {
+        print "Erreur !:" . $e->getMessage() . "<br/>";
+    }
 }
 
 // Supprime un réalisateur de la base de donnée identifier par son id
 function deleteFilmMaker($id)
 {
-
+    try {
+        $dbh = getPDO();
+        $query = 'SELECT * FROM filmmakers 
+                DELETE FROM filmmakers 
+                WHERE id = 4;';
+        $statement = $dbh->prepare($query); // prepare query
+        $statement->execute(); // execute query
+        $queryResult = $statement->fetchAll(PDO::FETCH_ASSOC); // prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        return null;
+    }
 }
 
 ?>
